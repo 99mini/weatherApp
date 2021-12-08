4662d2723c6fc07c3b60e4cbf67cf50954feee67
mini <itman390@gmail.com>
2021-12-08 18:07:09 +0900

    add max, min temperature and current time

--- main.js
+++ main.js
@@ -2,20 +2,31 @@ const API_KEY ="d8e6493d6b4d3895f2bdf26d8f616952";
 const CITY = "Seoul";
 const COUNTRY = "KR";
 
+const updateTimeDocument = document.getElementById("update-time");
+const cityDocument = document.getElementById("city");
+const countryDocument = document.getElementById("country");
+const temperatureDocument = document.getElementById("temperature");
+const descriptionDocument = document.getElementById("description");
+const weatherIconDocument = document.getElementById("weather-icon");
+const tempMaxDocument = document.getElementById("temp-max");
+const tempMinDocument = document.getElementById("temp-min");
+
 fetch(`http://api.openweathermap.org/data/2.5/weather?q=${CITY}, ${COUNTRY}&appid=${API_KEY}`)
 .then(response => response.json())
 .then(data => updateData(data))
 .catch(error => console.warn(error));
 
+
 function updateData(jsonData){
     console.log(jsonData);
 
-    const cityDocument = document.getElementById("city");
-    const countryDocument = document.getElementById("country");
-    const temperatureDocument = document.getElementById("temperature");
-    const descriptionDocument = document.getElementById("description");
-    const weatherIconDocument = document.getElementById("weather-icon");
-
+    let today = new Date();
+    let hours = today.getHours();
+    let mins = today.getMinutes() < 10 ? `0${today.getMinutes()}` : today.getMinutes();
+    let hourString =  hours > 12 ? "P.M." : "A.M." ;
+    
+    updateTimeDocument.innerText = `${hourString} ${hours}:${mins}`;
+    
     let cityData = jsonData["name"];
     let weahterData = jsonData["weather"];
     let countryData = jsonData["sys"]["country"];
@@ -25,19 +36,18 @@ function updateData(jsonData){
     
     let mainData = jsonData["main"];
     let temperatureData = mainData["temp"]; 
-    temperatureData -= 273.15;
-    temperatureData = temperatureData.toFixed(2);
+
+    let tempMaxData = mainData["temp_max"];
+    let tempMinData = mainData["temp_min"];
 
     cityDocument.innerText = cityData;
     countryDocument.innerText = countryData;
 
     weatherIconDocument.innerHTML = `<img src="icons/${weatherIcon}.png"/>`;
-    temperatureDocument.innerText = `${temperatureData}º`;
+    temperatureDocument.innerText = `${Math.round(temperatureData - 273.15)}ºC`;
     
     descriptionDocument.innerText = descriptionData;
 
-
-
-
-    
+    tempMaxDocument.innerText = `MAX: ${Math.round(tempMaxData - 273.15)}º`;
+    tempMinDocument.innerText = `MIN: ${Math.round(tempMinData - 273.15)}º`;
 }
